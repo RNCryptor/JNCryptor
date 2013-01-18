5de22f725aaae0084bc880b56cc4fbcddb25ad7b
duncan@wortharead.com <duncan@wortharead.com@ad231a64-f7e6-dc27-5c96-390962327f01>
2013-01-18 19:58:09 +0000

    Added licence information to files, plus additional meta data to POM file.

--- jncryptor/src/main/java/jncryptor/cryptography/AES256Ciphertext.java
+++ jncryptor/src/main/java/jncryptor/cryptography/AES256Ciphertext.java
@@ -1,3 +1,18 @@
+/*    Copyright 2013 Duncan Jones
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package jncryptor.cryptography;
 
 import org.apache.commons.lang3.Validate;
@@ -36,14 +51,11 @@ class AES256Ciphertext {
 
   AES256Ciphertext(byte[] data) throws InvalidDataException {
     Validate.notNull(data, "Data cannot be null.");
-    
+
     // Need the header to be able to determine the length
     if (data.length < HEADER_SIZE) {
       throw new InvalidDataException("Not enough data to read header.");
     }
-    
-    
-    
 
     if (data.length < MINIMUM_LENGTH_WITH_PASSWORD) {
       throw new InvalidDataException(String.format(
